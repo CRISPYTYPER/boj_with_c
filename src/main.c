52b419f2ac8f57aa669ffd1eb711fd3ce35aa2cc
CRISPYTYPER <jack3409@naver.com>
2023-07-16 21:50:12 +0900

    modify main.c due to c study

--- src/main.c
+++ src/main.c
@@ -1,21 +1,19 @@
+/*
+main.c
+*/
 #include <stdio.h>
-int max(int a, int b);
+char compare(char *str1, char *str2);
 int main() {
-    int a, b;
-    int (*pmax)(int, int);
-    pmax = max;
+    char str1[20];
+    char str2[20];
 
-    scanf("%d %d", &a, &b);
-    printf("max(a,b) : %d \n", max(a, b));
-    printf("pmax(a,b) : %d \n", pmax(a, b));
+    scanf("%s", str1);
+    scanf("%s", str2);
 
-    return 0;
-}
-int max(int a, int b) {
-    if (a > b)
-        return a;
-    else
-        return b;
-
+    if (compare(str1, str2)) {
+        printf("%s 와 %s 는 같은 문장 입니다. \n", str1, str2);
+    } else {
+        printf("%s 와 %s 는 다른 문장 입니다. \n", str1, str2);
+    }
     return 0;
 }
