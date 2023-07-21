942671eb110a67464f743ff165571fd784a0e66d
CRISPYTYPER <jack3409@naver.com>
2023-07-21 19:07:40 +0900

    solve 14425

--- src/p_14425.c
+++ src/p_14425.c
@@ -1,32 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char arr1[10000][501];
-char arr2[10000][501];
-int main() {
+#define BUCKET 10000
 
+typedef struct _Node *NodePointer;
+typedef struct _Node {
+    char data[501];
+    NodePointer link;
+} Node;
+
+NodePointer hash[BUCKET];
+
+int stringToInt(char *string) {
+    int sum = 0;
+    while (*string) {
+        sum += *string;
+        string++;
+    }
+    return sum;
+}
+
+int hashing(int key) { return key % BUCKET; }
+
+void chaining(NodePointer node) {
+    int index = hashing(stringToInt(node->data));
+
+    if (hash[index] == NULL) {
+        hash[index] = node;
+    } else {
+        node->link = hash[index];
+        hash[index] = node;
+    }
+}
+
+int searching(char *string) {
+    int index = hashing(stringToInt(string));
+
+    NodePointer cur = hash[index];
+    while (cur != NULL) {
+        if (!strcmp(cur->data, string))
+            return 1;
+        cur = cur->link;
+    }
+    return 0;
+}
+
+int main(void) {
     int n, m;
-    int cnt = 0;
-    char check2[10000] = {0};
     scanf("%d %d", &n, &m);
-    for (int i = 0; i < n; i++) {
-        scanf("%s", arr1[i]);
-    }
-    for (int i = 0; i < m; i++) {
-        scanf("%s", arr2[i]);
-    }
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (check2[j] == 1) {
-                continue;
-            }
-            if (!strcmp(arr1[i], arr2[j])) { // 두 문자열이 같으면
-                cnt++;
-                check2[j] = 1;
-            }
+        NodePointer newNode = (NodePointer)malloc(sizeof(Node));
+        scanf("%s", newNode->data);
+        newNode->link = NULL;
+        chaining(newNode);
+    }
+
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        char string[501];
+        scanf("%s", string);
+
+        if (searching(string)) {
+            count++;
         }
     }
-    printf("%d\n", cnt);
+    printf("%d\n", count);
+
     return 0;
 }
